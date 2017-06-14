043c4249ea58d138ae6473af2ef5dfa9f835008c
Matteo Capitanio <matteo.capitanio@cdp.it>
2017-06-14 07:21:53 +0200

    Refactored scripts

--- bin/start-impala.sh
+++ bin/start-impala.sh
@@ -12,3 +12,20 @@ supervisorctl start impala-state-store
 supervisorctl start impala-catalog
 supervisorctl start impala-server
 
+./wait-for-it.sh localhost:21050 -t 120
+./wait-for-it.sh localhost:24000 -t 120
+./wait-for-it.sh localhost:25010 -t 120
+rc=$?
+if [ $rc -ne 0 ]; then
+    echo -e "\n---------------------------------------"
+    echo -e "     Impala not ready! Exiting..."
+    echo -e "---------------------------------------"
+    exit $rc
+fi
+
+
+echo -e "\n\n--------------------------------------------------------------------------------"
+echo -e "You can now access to the following Impala UIs:\n"
+echo -e "Impala State Store 		http://localhost:25010"
+echo -e "\nMantainer:   Matteo Capitanio <matteo.capitanio@gmail.com>"
+echo -e "--------------------------------------------------------------------------------\n\n"
