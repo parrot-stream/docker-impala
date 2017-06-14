35c0f07ad7471cec558e4ce048b2baac19cc7f11
Matteo Capitanio <matteo.capitanio@cdp.it>
2017-06-14 07:25:00 +0200

    Refactored scripts

--- bin/start-yarn.sh
+++ bin/start-yarn.sh
@@ -39,3 +39,15 @@ if [ $rc -ne 0 ]; then
     echo -e "--------------------------------------------"
     exit $rc
 fi
+
+echo -e "\n\n--------------------------------------------------------------------------------"
+echo -e "You can now access to the following Hadoop Web UIs:"
+echo -e ""
+echo -e "Hadoop - NameNode:			http://localhost:50070"
+echo -e "Hadoop - DataNode:			http://localhost:50075"
+echo -e "Hadoop - YARN Node Manager:		http://localhost:8042"
+echo -e "Hadoop - YARN Resource Manager:	http://localhost:8088"
+echo -e "Hadoop - YARN Application History:	http://localhost:8188"
+echo -e "Hadoop - MapReduce Job History:	http://localhost:19888/jobhistory"
+echo -e "\nMantainer:   Matteo Capitanio <matteo.capitanio@gmail.com>"
+echo -e "--------------------------------------------------------------------------------\n\n"
